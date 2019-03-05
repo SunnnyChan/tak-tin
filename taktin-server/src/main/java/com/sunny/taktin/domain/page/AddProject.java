acec7fa0a3169e3b8901b03ed5c6ab992bd351c5
sunnychan <sunnnychan@gmail.com>
2019-03-05 11:27:35 +0800

    sunnychan

--- taktin-server/src/main/java/com/sunny/taktin/domain/page/AddProject.java
+++ taktin-server/src/main/java/com/sunny/taktin/domain/page/AddProject.java
@@ -4,6 +4,8 @@ package com.sunny.taktin.domain.page;
  * Created by sunnnychan@gmail.com on 2019/3/5.
  */
 
-public class AddProject {
+import com.sunny.rw.server.domain.page.PageBase;
+
+public class AddProject extends PageBase {
 
 }
