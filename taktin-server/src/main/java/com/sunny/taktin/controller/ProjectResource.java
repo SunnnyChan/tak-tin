81a4f030ba1a3336f348c431103e9b09c68e148b
sunnychan <sunnnychan@gmail.com>
2019-03-05 11:05:30 +0800

    sunnychan

---
+++ taktin-server/src/main/java/com/sunny/taktin/controller/ProjectResource.java
@@ -0,0 +1,9 @@
+package com.sunny.taktin.controller;
+
+/*
+ * Created by sunnnychan@gmail.com on 2019/3/5.
+ */
+
+public class ProjectResource {
+
+}
---
+++ taktin-server/src/main/java/com/sunny/taktin/domain/page/AddProject.java
@@ -0,0 +1,8 @@
+package com.sunny.taktin.domain.page;
+
+/*
+ * Created by sunnnychan@gmail.com on 2019/3/5.
+ */
+
+public class AddProject {
+}
